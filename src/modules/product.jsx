ddae65224e50ca87c74b7d5edeeb467903350683
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-07-30 15:30:57 +0200

    add setup to load and display items

---
+++ src/modules/product.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+
+const Product = ({product}) => {
+  return(
+    <div className="product-card">
+      <img src={product.image} alt={`Image of ${product.title}`} />
+      <h3>{product.title || `No Title found`}</h3>
+      <p>{product.price || `No Price found`}</p>
+    </div>
+  )
+}
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    image: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+    ])
+  })
+}
+
+export default Product
--- src/routes/store.jsx
+++ src/routes/store.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
+import Product from "../modules/product";
+
+const useStoreData = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => {
+        if(response.status >= 400){
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setProducts(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false))
+    }, []);
+    return { products, error, loading }; 
+}
+
 const Store = () => {
+  const { products, error, loading } = useStoreData(); 
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>A network error was encountered</p>
   return(
-    <h1>This is the Store</h1>
+    <>
+      <h1>This is the Store</h1>
+      <div className="products">
+        {products.map((product) => {
+          return <Product product={product} key={product.id}/>
+        })}
+      </div>
+    </>
   )
 }
 
