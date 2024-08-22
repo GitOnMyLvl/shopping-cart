1ac6aa22698b20fd7b6e03a3b41f3f182b637b6d
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-08-22 13:24:30 +0200

    small improvements

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -14,22 +14,23 @@ const Cart = () => {
 
   return (
     <section className="max-container">
-      <h1 className="cart-heading">Products in your cart</h1>
+
       {cart.length === 0 ? (
         <>
           <h2>Your cart is currently empty!</h2>
-          <NavLink to={`/store`}>Go to Store</NavLink>
+          <NavLink className="orange-text" to={`/store`}>Go to Store</NavLink>
         </>
       ) : (
         <>
+          <h1 className="cart-heading">Products in your cart</h1>
           <ul className="cart-list">
             {cart.map((product, index) => (
               <li key={index} className="cart-list-item">
                 <div className="cart-item">
                   <div className="cart-item-content">
-                    <img  className="cart-list-img" src={product.image} alt="" width={100} height={100} />
+                    <img className="cart-list-img" src={product.image} alt="" width={100} height={100} />
                     <div className="cart-text-with-btn">
-                      {product.quantity} x {product.title} - {parseFloat(product.price * product.quantity).toFixed(2)}€ <button className="delete-btn"  onClick={() => handleDelete(index)}><FontAwesomeIcon  icon={faTrash}/></button>
+                      {product.quantity} x {product.title} - {parseFloat(product.price * product.quantity).toFixed(2)}€ <button className="delete-btn" onClick={() => handleDelete(index)}><FontAwesomeIcon icon={faTrash} /></button>
                     </div>
                   </div>
                 </div>
@@ -38,9 +39,9 @@ const Cart = () => {
           </ul>
           <div className="checkout-area">
             <p className="total">Total price: {parseFloat(total).toFixed(2)}€</p>
-            <button  className="orange-btn checkout-btn">Checkout</button>
+            <button className="orange-btn checkout-btn">Checkout</button>
           </div>
-          
+
         </>
       )}
 
